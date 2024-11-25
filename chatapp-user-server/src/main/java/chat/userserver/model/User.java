7b56e420010352c07fd07f438ed0378805cbb0ec
hyunjae <dlguswo0504@naver.com>
2024-11-26 02:00:34 +0900

    User Service - 카프카 이벤트 발행, 세션로직 수정

--- chatapp-user-server/src/main/java/chat/userserver/model/User.java
+++ chatapp-user-server/src/main/java/chat/userserver/model/User.java
@@ -3,11 +3,12 @@ package chat.userserver.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
