a6576882c8a55a1f69c363f764a85917c91dce6b
hyunjae <dlguswo0504@naver.com>
2024-11-29 03:36:05 +0900

    [User Service] - 도메인 이벤트 클래스 코드 리팩토링 - 주석 추가

--- chatapp-user-server/src/main/java/chat/userserver/event/UserEvent.java
+++ chatapp-user-server/src/main/java/chat/userserver/event/UserEvent.java
@@ -6,11 +6,28 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * 사용자 이벤트를 나타내는 클래스.
+ * 이벤트는 CREATE, UPDATE, DELETE 작업을 포함.
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserEvent {
-    private String action; // CREATE, UPDATE, DELETE
+
+    // 이벤트 종류를 상수로 정의하여 일관성 유지
+    public static final String ACTION_CREATE = "CREATE";
+    public static final String ACTION_UPDATE = "UPDATE";
+    public static final String ACTION_DELETE = "DELETE";
+
+    /**
+     * 이벤트의 종류.
+     */
+    private String action;
+
+    /**
+     * 이벤트와 관련된 사용자 정보.
+     */
     private User user;
 }
--- chatapp-user-server/src/main/java/chat/userserver/model/User.java
+++ chatapp-user-server/src/main/java/chat/userserver/model/User.java
@@ -1,23 +1,55 @@
 package chat.userserver.model;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
-@Data
+/**
+ * 사용자 정보를 나타내는 엔티티 클래스.
+ * 데이터베이스의 'users' 테이블과 매핑됨.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
 
+    /**
+     * 사용자 고유 식별자.
+     * 자동으로 생성되며, 기본 키로 설정됨.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * 사용자의 고유한 이름.
+     * 중복을 허용하지 않으며, null 값이 될 수 없음.
+     */
     @Column(unique = true, nullable = false)
     private String username;
 
+    /**
+     * 사용자의 비밀번호.
+     * null 값이 될 수 없으며, 보안을 위해 적절히 암호화하여 저장해야 함.
+     */
     @Column(nullable = false)
     private String password;
-}
 
+    /**
+     * 객체의 문자열 표현을 커스터마이징.
+     * 비밀번호는 출력하지 않도록 설정.
+     * @return 사용자 정보를 포함한 문자열
+     */
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
